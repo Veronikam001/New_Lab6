4f04d661fc8de6673fd7ca1f8d096976900af4d9
tcorn <tcornwell@ufl.edu>
2023-03-07 22:17:22 -0500

    Added a decode function. In the if dunder, added a variable called 'password' to store user input password. Added "if x == '2'" for decode and "if x == '3'" to exit program.
    -Tyler Cornwell

--- veronika_matos_main.py
+++ veronika_matos_main.py
@@ -24,10 +24,37 @@ def encode(password):
             new_pass += '2'
     return new_pass
 
+def decode(password):                   # Decode functions the same as the encode function.
+    new_pass = ''
+    for elem in password[::]:
+        if elem == '0':
+            new_pass += '3'
+        if elem == '1':
+            new_pass += '4'
+        if elem == '2':
+            new_pass += '5'
+        if elem == '3':
+            new_pass += '6'
+        if elem == '4':
+            new_pass += '7'
+        if elem == '5':
+            new_pass += '8'
+        if elem == '6':
+            new_pass += '9'
+        if elem == '7':
+            new_pass += '0'
+        if elem == '8':
+            new_pass += '1'
+        if elem == '9':
+            new_pass += '2'
+
+    return new_pass
+
 
 if __name__ == '__main__':
     print('Menu', '\n-------------', '\n1. Encode', '\n2. Decode', '\n3. Quit', '\n')
     x = input('Please enter an option:')
+    password = 'a'      # Created a password variable to store password input by user
     while x != 'q':
         if x == '1':
             password = input('Please enter your password to encode:')
@@ -37,3 +64,10 @@ if __name__ == '__main__':
             x = input('Please enter an option:')
             # Added the first comment
             # Additional comment for 2nd change
+        if x == '2':        # Actual decode happens here, prints the encoded password with defined decode, then prints original password
+            decode(password)
+            print(f'The encoded password is {decode(password)}, and the original password is {password}')
+            print('Menu', '\n-------------', '\n1. Encode', '\n2. Decode', '\n3. Quit', '\n')
+            x = input('Please enter an option: ')
+        if x == '3':    # Added a quit function
+            quit()
